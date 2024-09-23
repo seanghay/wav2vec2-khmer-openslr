23fe2cb29a83b2992009b63445048d6c53f479f2
Seanghay Yath <seanghay.dev@gmail.com>
2024-09-23 12:53:47 +0700

    add another sample

--- inference.py
+++ inference.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
   model = Wav2Vec2ForCTC.from_pretrained("./model")
   processor = Wav2Vec2Processor.from_pretrained("./model")
 
-  y, sr = librosa.load("samples/khm_1161_1980987674.wav", mono=True, sr=16000)
+  y, sr = librosa.load("samples/audio.wav", mono=True, sr=16000)
   values = processor(y, sampling_rate=sr, return_tensors="pt")
 
   with torch.no_grad():
     outputs = model(**values).logits
     text = processor.batch_decode(outputs.argmax(dim=-1))[0]
     print(text)
-    # ប្អូន ប្រុស របស់ ខ្ញុំ បាន ទៅ លេង នៅ ទីក្រុង ម៉ានីល
+    # វិធាណការ៍ ដាប បំរាំ គួរចរ ក្នុង រាជធានី ភ្នំពេញដើម បី ស្វត្ឋិភាព សហគម បាន អនប័តរ យ៉ាង រ្លូន
